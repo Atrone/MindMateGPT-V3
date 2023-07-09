ec87760dec236b4b3c776e5b71d7cacd47fca2c2
anthony <anthony.teixeira55@gmail.com>
2023-07-08 21:55:34 -0400

    for insider

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ async def handle_payment(data: PaymentData):
             key = generate_key(5)
             # Store the key in Redis
             redis_client.set(key, 'true')
-            return f"Here is your key: {key}. \n\n\n IMPORTANT: Please store this key for future use. You input it to access the premium features."
+            return f"Here is your key: {key}. IMPORTANT: Please store this key for future use. You input it to access the premium features."
         else:
             return {"status": "error", "message": "Payment failed"}
 

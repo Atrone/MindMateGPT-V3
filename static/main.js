ecb8a5f15b28117158c6894b8eee101da75c3104
anthony <anthony.teixeira55@gmail.com>
2023-07-19 12:50:07 -0400

    major refactor second commit

--- static/main.js
+++ static/main.js
@@ -286,7 +286,7 @@ async function postData(data, key_data) {
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
-    key_body: JSON.stringify(key_data), // body data type must match "Content-Type" header
+    key_body: JSON.stringify(key_data) // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }

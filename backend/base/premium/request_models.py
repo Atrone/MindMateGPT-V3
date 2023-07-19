8cc476c74e9694dc2fc147c307639390103eb7d1
anthony <anthony.teixeira55@gmail.com>
2023-07-19 13:37:04 -0400

    final commit

--- backend/base/premium/request_models.py
+++ backend/base/premium/request_models.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel
 
 class InsightBody(BaseModel):
     recipient: str
+    key: str
--- backend/base/premium/service.py
+++ backend/base/premium/service.py
@@ -1,32 +1,32 @@
-
 async def summarize_text(openai, text):
     prompt = f"Summarize the following text in 5 sentences:\n{text}"
     response = openai.Completion.create(
-            engine="gpt-4",
-            prompt=prompt,
-            temperature=0.3,
-            max_tokens=3500,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=1
+        engine="gpt-4",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=3500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=1
     )
 
     return response["choices"][0]["text"]
 
+
 async def create_insights(openai, text):
     prompt = f"Here is a completed therapy session:" \
-                 f"\n\n{text}\n\n " \
-                 f"For the above completed session, " \
-                 f"provide a summary of the session as well as expert level insights into what a good next step for " \
-                 f"the patient would be."
+             f"\n\n{text}\n\n " \
+             f"For the above completed session, " \
+             f"provide a summary of the session as well as expert level insights into what a good next step for " \
+             f"the patient would be."
     response = openai.Completion.create(
-            engine="gpt-4",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=1000,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=1
+        engine="gpt-4",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=1
     )
 
     return response["choices"][0]["text"]

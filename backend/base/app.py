bbb868a9a5f85d238bef1b99182938d7af034f10
Anthony Teixeira <35811311+Atrone@users.noreply.github.com>
2023-07-28 13:31:23 -0400

    email

--- backend/base/app.py
+++ backend/base/app.py
@@ -4,6 +4,20 @@ import smtplib
 from typing import Dict
 from fastapi import APIRouter
 
+import time
+
+def send_email(retries=3, delay=5):
+    for i in range(retries):
+        try:
+            # your email sending code here...
+            return
+        except smtplib.SMTPServerDisconnected as e:
+            if i < retries - 1:
+                time.sleep(delay) # wait for a bit before trying again
+                                                                                        continue
+                                                                                                    else: # this was the last attempt
+                                                                                                                    raise e from None # re-raise the last exception
+
 
 class BaseApp:
     def __init__(self, redis_client, openai):
@@ -21,15 +35,20 @@ class BaseApp:
         return user_data
 
     async def send_email(self, recipient: str, message: str) -> Dict:
-        try:
-            server = smtplib.SMTP("smtp.gmail.com", 587, timeout=120)
-            server.starttls()
-            server.login(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"))
-            message = 'Subject: {}\n\n{}'.format("Therapy Insights from MindMateGPT Premium :)", message)
-            server.sendmail(os.getenv("SENDER_EMAIL"), recipient, message)
-            server.quit()
+        for attempt in range(5):
+            try:
+                server = smtplib.SMTP("smtp.gmail.com", 587, timeout=120)
+                server.starttls()
+                server.login(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"))
+                message = 'Subject: {}\n\n{}'.format("Therapy Insights from MindMateGPT Premium :)", message)
+                server.sendmail(os.getenv("SENDER_EMAIL"), recipient, message)
+                server.quit()
 
-            return {"message": "Email sent successfully"}
+                return {"message": "Email sent successfully"}
 
-        except Exception as e:
-            return {"message": f"Failed to send email: {str(e)}"}
+            except Exception as e:
+                if attempt < 4:  # If it's not the last attempt
+                    time.sleep(10)  # Wait for 10 seconds before the next attempt
+                    continue
+                else:
+                    return {"error": str(e)}

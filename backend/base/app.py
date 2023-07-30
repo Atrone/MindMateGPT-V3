c6ab1d3abb50c4418b056bbf4ccc5b2d97cc2b6c
Anthony Teixeira <35811311+Atrone@users.noreply.github.com>
2023-07-30 11:28:55 -0400

    Update app.py

--- backend/base/app.py
+++ backend/base/app.py
@@ -25,7 +25,7 @@ class BaseApp:
     async def send_email(self, recipient: str, message: str) -> Dict:
         for attempt in range(10):
             try:
-                server = smtplib.SMTP("smtp.gmail.com", 587, timeout=120)
+                server = smtplib.SMTP("smtp.mailgun.org", 587, timeout=120)
                 server.starttls()
                 server.login(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"))
                 message = 'Subject: {}\n\n{}'.format("Therapy Insights from MindMateGPT Premium :)", message)

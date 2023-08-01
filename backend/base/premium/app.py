5e0511871fcf10e7993b73dfa7f78025abf80d55
anthony <anthony.teixeira55@gmail.com>
2023-08-01 07:33:05 -0400

    test new mail

--- backend/base/premium/app.py
+++ backend/base/premium/app.py
@@ -5,6 +5,7 @@ from backend.base.premium.service import create_insights
 from backend.base.premium.request_models import InsightBody
 from backend.tasks import send_email_task
 
+
 class PremiumApp(BaseApp):
     def __init__(self, redis_client, openai):
         super().__init__(redis_client, openai)
@@ -17,7 +18,7 @@ class PremiumApp(BaseApp):
 
                 message = user_data[session_id]['transcript'] + "\n\n\n\n"
 
-                send_email_task.delay(body.recipient, message, self.openai, user_data[ session_id]['transcript'])
+                send_email_task.delay(body.recipient, message, self.openai, user_data[session_id]['transcript'])
                 return "sent"
             else:
                 return None

5826dbffbcf34c6f348d093820f76f67e22e2a2d
Anthony Teixeira <35811311+Atrone@users.noreply.github.com>
2023-07-31 21:26:04 -0400

    stuff

--- backend/base/premium/app.py
+++ backend/base/premium/app.py
@@ -3,41 +3,7 @@ from backend.auth.keys import check_key
 from backend.base.app import BaseApp
 from backend.base.premium.service import create_insights
 from backend.base.premium.request_models import InsightBody
-from celery import Celery
-import os
-
-def make_celery(app_name=__name__):
-    backend = broker = os.getenv('REDIS_URL')
-    return Celery(app_name, backend=backend, broker=broker)
-
-celery = make_celery()
-
-@celery.task
-def send_email_task(recipient, message):
-    mailertogo_host = os.environ.get('MAILERTOGO_SMTP_HOST')
-    mailertogo_port = os.environ.get('MAILERTOGO_SMTP_PORT', 587)
-    mailertogo_user = os.environ.get('MAILERTOGO_SMTP_USER')
-    mailertogo_password = os.environ.get('MAILERTOGO_SMTP_PASSWORD')
-    mailertogo_domain = os.environ.get('MAILERTOGO_DOMAIN', "mydomain.com")
-    sender_user = 'noreply'
-    sender_email = "@".join([sender_user, mailertogo_domain])
-
-    for attempt in range(3):
-        try:
-            server = smtplib.SMTP("smtp.gmail.com", 587)
-            server.starttls()
-            server.login(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"))
-            message = 'Subject: {}\n\n{}'.format("Therapy Insights from MindMateGPT Premium :)", message)
-            server.sendmail(os.getenv("SENDER_EMAIL"), recipient, message)
-            server.quit()
-            return {"message": "Email sent successfully"}
-
-        except Exception as e:
-            print(e)
-            time.sleep(10)
-            continue
-    return {"message": "Email not sent successfully"}
-
+from backend.tasks import send_email_task
 
 class PremiumApp(BaseApp):
     def __init__(self, redis_client, openai):

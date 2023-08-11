6d55b302dc453a507b16a2c3706145e8066fbe8a
anthony <anthony.teixeira55@gmail.com>
2023-08-11 10:59:25 -0400

    MBTI!

--- static/form/controller/formController.js
+++ static/form/controller/formController.js
@@ -49,7 +49,10 @@ class ContactFormController {
 
     handleSubmit(event) {
         event.preventDefault();
-        document.cookie = `mbti=${encodeURIComponent(this.model.mbti)}; path=/; max-age=86400 * 7 * 30`;  // The result is stored for 1 day (86400 seconds)
+       if (!mbtiCookieValue) {
+            document.cookie = `mbti=${encodeURIComponent(this.model.mbti)}; path=/; max-age=86400 * 7 * 30`;  // The result is stored for 1 day (86400 seconds)
+        }
+
         this.service.validateAndSubmit(this.model);
     }
     handleSkip(event) {

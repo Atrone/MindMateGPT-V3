6af13bb5e17277d2caa2154c7d37a6a1e2ced8c8
anthony <anthony.teixeira55@gmail.com>
2023-08-12 17:45:39 -0400

    give em a chance

--- backend/base/free/service.py
+++ backend/base/free/service.py
@@ -45,7 +45,7 @@ class FreeAppService:
             return "I'm really sorry that you're feeling this way, but I'm unable to provide the help that you need. " \
                    "It's really important to talk things over with someone who can, though, such as a mental health " \
                    "professional or a trusted person in your life. You may also see our disclaimer for a crisis " \
-                   "hotline. ", []
+                   "hotline. ", conversation
 
         try:
             response = self.openai.ChatCompletion.create(

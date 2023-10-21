55550798725736c314bb8987c17190638772b7a9
anthony <anthony.teixeira55@gmail.com>
2023-10-21 01:21:45 -0400

    buy

--- backend/base/premium/app.py
+++ backend/base/premium/app.py
@@ -1,8 +1,13 @@
-from fastapi import Request
+import stripe
+from fastapi import Request, Header
 from backend.base.app import BaseApp
 from backend.base.premium.request_models import InsightBody
 from backend.tasks import send_email_task
 from celery.result import AsyncResult
+import os
+from sse_starlette.sse import EventSourceResponse
+
+clients = []
 
 
 class PremiumApp(BaseApp):
@@ -25,3 +30,36 @@ class PremiumApp(BaseApp):
             if task.ready():
                 return {"status": "completed", "result": task.result}
             return {"status": "pending"}
+
+        @self.router.get("/events")
+        async def events(request: Request):
+            async def event_stream():
+                while True:
+                    if clients:
+                        data = clients.pop()
+                        yield f"data: {data}\n\n"
+
+            return EventSourceResponse(event_stream())
+
+        @self.router.post("/webhook")
+        async def webhook_received(request: Request, stripe_signature: str = Header(None)):
+            webhook_secret = os.environ["STRIPE_WEBHOOK_SECRET"]
+            data = await request.body()
+            try:
+                event = stripe.Webhook.construct_event(
+                    payload=data,
+                    sig_header=stripe_signature,
+                    secret=webhook_secret
+                )
+            except Exception as e:
+                return {"error": str(e)}
+
+            event_type = event['type']
+            if event_type == 'checkout.session.completed' or event_type == 'invoice.paid':
+                clients.append("payment_success")
+            elif event_type == 'invoice.payment_failed':
+                print('invoice payment failed')
+            else:
+                print(f'unhandled event: {event_type}')
+
+            return {"status": "success"}

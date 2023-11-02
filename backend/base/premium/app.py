14c4e100f0ed9637691752e3002742c2fbd59451
anthony <anthony.teixeira55@gmail.com>
2023-11-01 21:00:18 -0400

    cuppa features

--- backend/base/premium/app.py
+++ backend/base/premium/app.py
@@ -1,5 +1,6 @@
 from fastapi import Request
 from backend.base.app import BaseApp
+from backend.base.entities import UserSessionData
 from backend.base.premium.request_models import InsightBody
 from backend.tasks import send_email_task
 from celery.result import AsyncResult
@@ -13,11 +14,11 @@ class PremiumApp(BaseApp):
         @self.router.post("/download")
         async def download_insights(request: Request, body: InsightBody):
             session_id = request.headers['Session']
-            user_data = await self.get_user_data_dict(session_id)
+            user_data_dict = await self.get_user_data_dict(session_id)
+            user_data = UserSessionData(**user_data_dict)
+            message = UserSessionData.transcript + "\n\n\n\n"
 
-            message = user_data[session_id]['transcript'] + "\n\n\n\n"
-
-            task = send_email_task.delay(body.recipient, message, user_data[session_id]['transcript'])
+            task = send_email_task.delay(body.recipient, message, UserSessionData.transcript)
             return {"task_id": task.id}
 
         @self.router.get("/task_status/{task_id}")

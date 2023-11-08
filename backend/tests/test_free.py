e350e5fec3deae9111d5b42a1b9390229cd92cbc
anthony <anthony.teixeira55@gmail.com>
2023-11-08 13:46:53 -0500

    better

--- backend/tests/test_free.py
+++ backend/tests/test_free.py
@@ -3,21 +3,26 @@ from backend.app import app
 import unittest
 
 
+# Create a fixture for the test client that can be reused in multiple tests
+def setUpModule():
+    global client
+    client = TestClient(app)
+
+
 class TestFreeApp(unittest.TestCase):
 
     def test_get_form_endpoint(self):
-        try:
-            client = TestClient(app)
-            response = client.post("/api/getForm", headers={"Session": "test_session", 'taskResult': 'summary: hi, insight: bruh'})
-            assert response.json()['childhood'] == "Not provided" and response.json()['prompt'] != "" and response.json()['summary'] == ": hi,"
-        except Exception as e:
-            assert 'int() argument must be a string' in e.args[0]
+        # No need for try-except here, let the test framework handle the exception
+        response = client.post("/api/getForm", headers={"Session": "test_session", 'taskResult': 'summary: hi, '
+                                                                                                 'insight: bruh'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('childhood', response.json())
+        self.assertEqual(response.json()['childhood'], "Not provided")
+        self.assertNotEqual(response.json()['prompt'], "")
+        self.assertIn('summary', response.json())
+        self.assertEqual(response.json()['summary'], ": hi,")
 
     def test_get_response_endpoint(self):
-        try:
-            client = TestClient(app)
-            response = client.post("/api/therapistGPT", headers={"Session": "test_session", 'taskResult': 'task'},
-                                   json={"message": "hi"})
-            assert response.json() == "Something is wrong with our AI provider"
-        except Exception as e:
-            assert 'int() argument must be a string' in e.args[0]
+        response = client.post("/api/therapistGPT", headers={"Session": "test_session"}, json={"message": "hi"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), "Something is wrong with our AI provider")

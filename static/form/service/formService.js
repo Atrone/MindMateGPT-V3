b69e794257824e4053434c67209a54b1bf897cb4
Your New Username <anthlift@gmail.com>
2024-03-12 15:31:09 -0400

    new feature who dis

--- static/form/service/formService.js
+++ static/form/service/formService.js
@@ -11,7 +11,7 @@ class ContactFormServices {
     }
 
     submitForm(model) {
-        const url = 'https://mindmategpt.com/api/getForm';
+        const url = `https://${window.location.hostname}/api/getForm`;
         const formData = new FormData();
 
         formData.append('mbti', model.mbti);
